225aacc628ec99159cafc909c718af1f2bb2e1d9
Federico <fedealtava1992@gmail.com>
2025-03-23 17:11:26 +0100

    bases types

--- bases/app.ts
+++ bases/app.ts
@@ -1,17 +1,17 @@
 (() => {
 
     // Tipos
-    const batman = 'Bruce';
-    const superman = 'Clark';
+    const batman:string = 'Bruce';
+    const superman:string = 'Clark';
   
-    const existe = false;
+    const existe:boolean = false;
   
     // Tuplas
-    const parejaHeroes = [batman,superman];
-    const villano = ['Lex Lutor',5,true];
+    const parejaHeroes:[ string, string ] = [batman,superman];
+    const villano:[string, number, boolean] = ['Lex Lutor',5,true];
   
     // Arreglos
-    const aliados = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+    const aliados:string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
   
     //Enumeraciones
     const fuerzaFlash = 5;
@@ -20,17 +20,17 @@
     const fuerzaAcuaman = 0;
   
     // Retorno de funciones
-    function activar_batiseñal(){
+    function activar_batiseñal():string{
       return 'activada';
     }
   
-    function pedir_ayuda(){
+    function pedir_ayuda():void{
       console.log('Auxilio!!!');
     }
   
     // Aserciones de Tipo
-    const poder: any = '100';
-    const largoDelPoder:number = poder.length;
+    const poder: any = '100' as string;
+    const largoDelPoder:number = poder.length as number;
     console.log( largoDelPoder );
   
   
